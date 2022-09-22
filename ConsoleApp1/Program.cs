4c93cfd014128c64508648a3c877b8170cf2e4cf
hyunjae0912 <114149634+hyunjae0912@users.noreply.github.com>
2022-09-22 21:14:27 +0900

    2022_09_22

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //클래스 불러옴
+            player player = new player();
+            monster monster = new monster();
+
+
+            Console.WriteLine(monster.at);
+            Console.WriteLine( player.att);
+
+            Console.ReadLine();
+
+
+            //player의 AddAt의 함수에서 10추가함
+            player.AddAt(10);
+
+            player.fight(monster.at);
+
+            Console.WriteLine(player.att);
+            Console.ReadLine();
+
+            player.hp -= monster.at;
+
+
+            
+
+        }
+      
+    }
+
+}
---
+++ ConsoleApp1/player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace ConsoleApp1
+{
+    public class player
+    {
+        public int att=10;
+        public int hp=100;
+
+        public int AddAt(int a)
+        {
+            //공격력 증가
+            a = 10;
+            //att up
+            att += a;
+            return (att);
+        }
+
+
+        public void fight(int monsteratt)
+        {
+            //player의hp가 선언 되있으니 player 생략
+            hp -= monsteratt;
+        }
+    }
+}
